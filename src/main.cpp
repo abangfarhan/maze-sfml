32593f545e8efb5aef9e4a9b6dedf4efe6e92603
Abang Farhan <abangfarhan31@gmail.com>
2018-02-06 18:26:16 +0700

    Fix many things. Now the maze generation is working (using 'recursive backtracking')

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,43 @@
 #include <time.h>
 #include <SFML/Graphics.hpp>
 
-#define right 0
-#define down 1
-#define left 2
-#define top 3
+#define side_right 0
+#define side_down 1
+#define side_left 2
+#define side_top 3
 
-const int gridWidth = 20;
-const int gridHeight = 10;
+const int gridWidth = 40;
+const int gridHeight = 20;
 
 struct Node
 {
-    // right, down, left, top
+    // side_right, side_down, side_left, side_top
     bool walls[4] = { true, true, true, true };
     bool visited = false;
 };
 
 int getNext_i(int current_i, int side)
 {
-    if ((side == right && current_i == gridWidth - 1) || (side == left && current_i == 0))
+    if ((side == side_right && current_i == gridWidth - 1) || (side == side_left && current_i == 0))
         return -1;
 
     int delta_i = 0;
-    if (side == right)
+    if (side == side_right)
         delta_i++;
-    else if (side == left)
+    else if (side == side_left)
         delta_i--;
     return current_i + delta_i;
 }
 
 int getNext_j(int current_j, int side)
 {
-    if ((side == down && current_j == gridHeight - 1) || (side == top && current_j == 0))
+    if ((side == side_down && current_j == gridHeight - 1) || (side == side_top && current_j == 0))
         return -1;
 
     int delta_j = 0;
-    if (side == down)
+    if (side == side_down)
         delta_j++;
-    else if (side == top)
+    else if (side == side_top)
         delta_j--;
     return current_j + delta_j;
 }
@@ -53,30 +53,29 @@ void setWall(Node nodeList[], int i, int j, int side, bool state)
     nodeList[i + j * gridWidth].walls[side] = state;
     int oppositeSide = (side + 2) % 4;
     nodeList[next_i + next_j * gridWidth].walls[oppositeSide] = state;
-    // if ((side == right && i < gridWidth - 1) ||
-    //     (side == left  && i > 1) ||
-    //     (side == down  && j < gridHeight - 1) ||
-    //     (side == top   && j > 1))
+    // if ((side == side_right && i < gridWidth - 1) ||
+    //     (side == side_left  && i > 1) ||
+    //     (side == side_down  && j < gridHeight - 1) ||
+    //     (side == side_top   && j > 1))
     // {
     //     nodeList[i + j * gridWidth].walls[side] = state;
     //     int oppositeSide = (side + 2) % 4;
     //     switch(side)
     //     {
-    //     case right:
+    //     case side_right:
     //         i++; break;
-    //     case down:
+    //     case side_down:
     //         j++; break;
-    //     case left:
+    //     case side_left:
     //         i--; break;
-    //     case top:
+    //     case side_top:
     //         j--; break;
     //     }
     //     nodeList[i + j * gridWidth].walls[oppositeSide] = state;
     // }
 }
 
-
-void drawNode(sf::RenderWindow* window, Node node, int i, int j, int nodeSizePx)
+void drawNode(sf::RenderWindow* window, Node node, int i, int j, int nodeSizePx, bool isCurrent = false)
 {
     if (!(node.walls[0] && node.walls[1] && node.walls[2] && node.walls[3]))
     {
@@ -88,28 +87,30 @@ void drawNode(sf::RenderWindow* window, Node node, int i, int j, int nodeSizePx)
         cell.setPosition(sf::Vector2f(i * nodeSizePx + wallThickness, j * nodeSizePx + wallThickness));
         cell.setSize(sf::Vector2f(innerNodeSizePx, innerNodeSizePx));
         cell.setFillColor(sf::Color::Black);
+        if (isCurrent)
+            cell.setFillColor(sf::Color::Blue);
 
         sf::RectangleShape wall;
-        wall.setFillColor(sf::Color(50, 50, 50));
-        if (!node.walls[right])
+        wall.setFillColor(sf::Color::Black);
+        if (!node.walls[side_right])
         {
             wall.setPosition(sf::Vector2f(i * nodeSizePx + wallThickness + innerNodeSizePx, j * nodeSizePx + wallThickness));
             wall.setSize(sf::Vector2f(wallThickness, innerNodeSizePx));
             window->draw(wall);
         }
-        if (!node.walls[left])
+        if (!node.walls[side_left])
         {
             wall.setPosition(sf::Vector2f(i * nodeSizePx, j * nodeSizePx + wallThickness));
             wall.setSize(sf::Vector2f(wallThickness, innerNodeSizePx));
             window->draw(wall);
         }
-        if (!node.walls[top])
+        if (!node.walls[side_top])
         {
             wall.setPosition(sf::Vector2f(i * nodeSizePx + wallThickness, j * nodeSizePx));
             wall.setSize(sf::Vector2f(innerNodeSizePx, wallThickness));
             window->draw(wall);
         }
-        if (!node.walls[down])
+        if (!node.walls[side_down])
         {
             wall.setPosition(sf::Vector2f(i * nodeSizePx + wallThickness, j * nodeSizePx + wallThickness + innerNodeSizePx));
             wall.setSize(sf::Vector2f(innerNodeSizePx, wallThickness));
@@ -120,7 +121,7 @@ void drawNode(sf::RenderWindow* window, Node node, int i, int j, int nodeSizePx)
     }
 }
 
-bool notAllNodesVisited(Node nodeList[])
+bool hasUnvisitedNodes(Node nodeList[])
 {
     for (int i = 0; i < gridWidth * gridHeight; ++i)
         if (!nodeList[i].visited)
@@ -128,21 +129,35 @@ bool notAllNodesVisited(Node nodeList[])
     return false;
 }
 
+bool hasUnvisitedNeighbor(Node nodeList[], int i, int j)
+{
+    for (int side = 0; side < 4; ++side)
+    {
+        int next_i = getNext_i(i, side);
+        int next_j = getNext_j(j, side);
+        if (next_i == -1 || next_j == -1) continue;
+        if (!nodeList[next_i + next_j * gridWidth].visited)
+            return true;
+    }
+    return false;
+}
+
 int main()
 {
     srand(time(NULL));
-    const int nodeSizePx = 50;
+    const int nodeSizePx = 25;
     const int screenWidth = gridWidth * nodeSizePx;
     const int screenHeight = gridHeight * nodeSizePx;
 
     Node nodeList[gridWidth * gridHeight];
 
-    setWall(nodeList, 1, 1, right, false);
-    setWall(nodeList, 2, 2, top, false);
+    // setWall(nodeList, 18, 8, side_right, false);
+    // setWall(nodeList, 19, 9, side_top, false);
 
-    int currentNode[2] = {0, 0};
-    nodeList[currentNode[0] + currentNode[1] + gridWidth].visited = true;
-    std::stack<int*> nodeStack;
+    int current_i = 0, current_j = 0;
+    nodeList[current_i + current_j * gridWidth].visited = true;
+    std::stack<int> nodeStack_i;
+    std::stack<int> nodeStack_j;
 
     sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "Maze");
     while(window.isOpen())
@@ -162,12 +177,40 @@ int main()
         //                 setWall(nodeList, i, j, side, !nodeList[i + j * gridWidth].walls[side]);
         //     }
 
+        if (hasUnvisitedNodes(nodeList))
+        {
+            if (hasUnvisitedNeighbor(nodeList, current_i, current_j))
+            {
+                int nextSide;
+                int next_i, next_j;
+                do {
+                    nextSide = rand() % 4;
+                    next_i = getNext_i(current_i, nextSide);
+                    next_j = getNext_j(current_j, nextSide);
+                } while (next_i == -1 || next_j == -1 ||
+                        nodeList[next_i + next_j * gridWidth].visited);
+                nodeStack_i.push(current_i);
+                nodeStack_j.push(current_j);
+                setWall(nodeList, current_i, current_j, nextSide, false);
+                current_i = next_i;
+                current_j = next_j;
+                nodeList[current_i + current_j * gridWidth].visited = true;
+            }
+            else
+            {
+                current_i = nodeStack_i.top();
+                current_j = nodeStack_j.top();
+                nodeStack_i.pop();
+                nodeStack_j.pop();
+            }
+        }
 
         for (int i = 0; i < gridWidth; ++i)
             for (int j = 0; j < gridHeight; ++j)
-                drawNode(&window, nodeList[i + j * gridWidth], i, j, nodeSizePx);
+                drawNode(&window, nodeList[i + j * gridWidth], i, j, nodeSizePx,
+                        (i == current_i && j == current_j));
 
-        sf::sleep(sf::milliseconds(500));
+        // sf::sleep(sf::milliseconds(100));
 
         window.display();
     }
